173e72d5271deeb67abb6eb0dca40cc3d6030d86
BJ (Bartek Jachym) <bartek.jachym12@gmail.com>
2021-12-14 21:29:46 +0100

    changess

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ app.get('/api/COlevel/', (req, res) =>
     res.send(sensors);
 });
 
-app.get('/api/COlevel/:id', (req, res) =>
-{
-    res.set({
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-    });
+// app.get('/api/COlevel/:id', (req, res) =>
+// {
+//     res.set({
+//         "Content-Type": "application/json",
+//         "Access-Control-Allow-Origin": "*",
+//     });
 
-    const sensor = sensors.find(c => c.id === parseInt(req.params.id));
-    if (!sensor) res.status(404).send('The sensor with this ID does not exist');
-    res.send(sensor);
+//     const sensor = sensors.find(c => c.id === parseInt(req.params.id));
+//     if (!sensor) res.status(404).send('The sensor with this ID does not exist');
+//     res.send(sensor);
   
-});
+// });
 
 //app.post();
 
@@ -32,15 +32,15 @@ app.put('/api/COlevel/:id', (req, res) =>
 {
     //check the sensor
     // if doesn't exist return 404
-    const sensor = sensors.find(c => c.id === parseInt(req.params.id));
-    if (!sensor) res.status(404).send('The sensor with this ID does not exist');
+  //  const sensor = sensors.find(c => c.id === parseInt(req.params.id));
+  //  if (!sensor) res.status(404).send('The sensor with this ID does not exist');
 
     //validate
-    //If invalid, return 400
-    if (!req.body.value || req.body.value > 100)
-    {
-        res.status(400).send('Wrong value of the sensor');
-    }
+   // If invalid, return 400
+   if (!req.body.value || req.body.value > 100)
+   {
+       res.status(400).send('Wrong value of the sensor');
+   }
       
     sensor.value = req.body.value;  //update COlevel
     res.status(200).send(sensor);    //return new COlevel
